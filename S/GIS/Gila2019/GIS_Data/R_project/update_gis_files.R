01bada2f8e3c255f478591072d445be221db5e8a
Ryan Gillespie <rsgillespie7@gmail.com>
2020-02-19 18:03:14 -0700

    Renamed variable, improved comments, and put file I/O at beginning

--- S/GIS/Gila2019/GIS_Data/R_project/update_gis_files.R
+++ S/GIS/Gila2019/GIS_Data/R_project/update_gis_files.R
@@ -1,7 +1,15 @@
+# This script was used to update GIS files with new observations entered in Excel sheets. 
+# To make the paths relative to the script location, it's intended to be run in RStudio.
+
+# Import libraries to deal with Excel sheets, spatial data
 library('xlsx')
 library('tidyverse')
 library('sf')
 
+#------------------------------
+# File I/O
+#------------------------------
+# Set working directory to script location
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Read in Excel files
@@ -20,7 +28,13 @@ ybcu_xl <- read.xlsx(file = "../../DataEntry/YBCU/YBCU_surveys_2019.xlsx",
                      colIndex = c(1:22),
                      as.data.frame = TRUE)
 
+# Output geopackage locations
+swflout <- "../SWFL_2019.gpkg"
+ybcuout <- "../YBCU_2019.gpkg"
 
+#------------------------------
+# Prepare observation tables
+#------------------------------
 # Clean up empty rows
 survey_xl <- filter(survey_xl, !is.na(Date))
 nests_xl <- filter(nests_xl, !is.na(Nest))
@@ -31,7 +45,6 @@ ybcu_presence <- filter(ybcu_xl, ybcu_xl$Presence.Absence == "P")
 ybcu_absence <- filter(ybcu_xl, ybcu_xl$Presence.Absence == "A")
 
 # Make sure coordinates are numeric
-# survey_xl$UTM.E <- as.numeric(levels(survey_xl$UTM.E)[survey_xl$UTM.E])
 survey_xl$UTM.E <- as.numeric(survey_xl$UTM.E)
 survey_xl$UTM.N <- as.numeric(survey_xl$UTM.N)
 
@@ -44,6 +57,9 @@ ybcu_presence$Projected.UTM.N <- as.numeric(ybcu_presence$Projected.UTM.N)
 ybcu_absence$Wpt_UTM_E <- as.numeric(ybcu_absence$Wpt_UTM_E)
 ybcu_absence$Wpt_UTM_N <- as.numeric(ybcu_absence$Wpt_UTM_N)
 
+#------------------------------
+# Create spatial datasets from tables
+#------------------------------
 # Plot coordinates spatially
 survey_sf <- st_as_sf(x = survey_xl,
                       coords = c("UTM.E", "UTM.N"),
@@ -61,12 +77,11 @@ ybcu_a_sf <- st_as_sf(x = ybcu_absence,
                       coords = c("Wpt_UTM_E", "Wpt_UTM_N"),
                       crs = "+proj=utm +zone=12 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
 
-# Output to geopackage
-outfile <- "../SWFL_2019.gpkg"
-ybcuout <- "../YBCU_2019.gpkg"
-
-st_write(survey_sf, dsn = outfile, layer = "survey_data", update = TRUE, layer_options = "OVERWRITE=YES")
-st_write(nests_sf, dsn = outfile, layer = "nests", update = TRUE, layer_options = "OVERWRITE=YES")
+#------------------------------
+# Write to geopackages
+#------------------------------
+st_write(survey_sf, dsn = swflout, layer = "survey_data", update = TRUE, layer_options = "OVERWRITE=YES")
+st_write(nests_sf, dsn = swflout, layer = "nests", update = TRUE, layer_options = "OVERWRITE=YES")
 
 st_write(ybcu_p_sf,dsn = ybcuout, layer = "presence", update = TRUE, layer_options = "OVERWRITE=YES")
 st_write(ybcu_a_sf,dsn = ybcuout, layer = "absence", update = TRUE, layer_options = "OVERWRITE=YES")
